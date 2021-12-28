0377ad6953b194a7ad42c398210bd911938896e8
tiagov29 <santiagovalenciav29@gmail.com>
2021-12-27 20:20:00 -0500

    :sparkles: creating the modules

---
+++ data/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    question: "which of the next option does not an code editor",
+    choices: ["vim", "vscode", "emacs", "word"],
+    answer: "word",
+  },
+  {
+    question: "what language is not object oriented",
+    choices: ["java", "Haskell", "c++", "python"],
+    answer: "Haskell",
+  },
+  {
+    question: "what language is not useful for providing style to the web site",
+    choices: ["stylus", "less", "postCss", "nextCss"],
+    answer: "postCss",
+  },
+];
